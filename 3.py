352ec28e77549ec6e2c9332f447d91ba268f5e71
seoyoons <83273261+seoyoons@users.noreply.github.com>
2021-05-08 23:39:23 +0900

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,11 @@
+x = "Hello"   # 변수 x에 문자 Hello 선언
+y = "Bye"     # 변수 y에 문자 Bye 선언
+z = "안녕하세요. 저는 21학번 새내기입니다."
+
+print(x)
+print(y)
+print(z)
+print("\n")       # 공백
+print(x+y+z)
+print("\n")      # 공백 
+print(z)
