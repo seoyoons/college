c51c2bde24d5c7bd7697df8af3206c1731a072e3
seoyoons <83273261+seoyoons@users.noreply.github.com>
2021-05-09 00:17:23 +0900

    Create 5-2.py

---
+++ 5-2.py
@@ -0,0 +1,19 @@
+x = 3
+
+# 1)
+if x > 2 :
+  print("Hello", "\n")      # x가 3보다 크면 Hello 출력
+
+# 2)
+if x > 5 :
+  print("Hello", "\n")      # x가 5보다 크다면 Hello, 그렇지 않으면 Hi
+else : 
+  print("Hi","\n")
+
+# 3)
+if x > 5 :
+  print("Hello","\n")       # x가 5보다 크면 Hello 출력
+elif x == 3 :
+  print ("Bye","\n")        # x가 3과 같다면 Bye 출력
+else:
+  print("Hi","\n")          # 그게 아니라면 Hi 출력
