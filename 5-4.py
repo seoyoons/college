802b750b70f449d4e0d738262f3b01da862417f8
seoyoons <83273261+seoyoons@users.noreply.github.com>
2021-05-09 00:35:02 +0900

    Create 5-4.py

---
+++ 5-4.py
@@ -0,0 +1,16 @@
+a = 3
+b = 2
+c = 5
+d = c - a
+
+if a > b :
+  print("Hello")       # a가 b보다 크면 Hello 출력
+
+if a > c :
+  print("What?")       # a가 c보다 크면 What? 출력
+
+elif a == d :
+  print("Bye")         # a가 d와 같으면 Bye 출력
+
+else :
+  print("Hi!")         
